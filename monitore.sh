05fc9acaff3fb75e58ac4e5bd9f6abebbde92fb9
foxx3r <coluna123@protonmail.ch>
2019-05-04 16:36:49 -0300

    Update monitore.sh
    Re-organização de linhas

--- monitore.sh
+++ monitore.sh
@@ -3,23 +3,28 @@
 cp -r ./monitor.sh /usr/bin/monitore
 echo 'execute o programa apenas digitando "monitore"!!!' 
 sleep 1
+
 echo "Telegram: @Foxxer_SA"
 echo "Se inscreve la @AcervoHackerBR!!!"
 echo "GitHub: foxx3r"
+
 sleep 1
 echo "instalando pacotes adicionais..."
 sudo apt-get install dstat > /dev/null 
 sleep 1
+
 echo "=============================="
 echo "Monitorando seu sistema..."
 echo "=============================="
 sleep 2
 free -m
+
 echo "=============================="
 echo "Obtemdo relatorios..."
 echo "=============================="
 sleep 2
 vmstat -S M
+
 echo "=============================="
 echo "Obtendo relatorios gerais..."
 echo "Pressione CTRL+C para parar"
